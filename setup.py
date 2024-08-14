8c777530f9a92f0bb8953495195a6337bec7b997
Jésica Collado <collado.jesica@gmail.com>
2024-08-13 22:19:37 -0300

    fix requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 import setuptools
+import os
+
+requirements = [
+    i.strip() for i in open(os.path.join("requirements", "base.txt")).readlines()
+]
 
 setuptools.setup(
     name="guido",
     version="0.1",
     packages=["guido"],
     entry_points={"console_scripts": ["guido=guido.__main__:main"]},
+    install_requires=requirements,
 )
